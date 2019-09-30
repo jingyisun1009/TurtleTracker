a832d072f32cc7620fb2b2d9b0d47c874a4810a6
jingyisun1009 <jingyi.sun@duke.edu>
2019-09-30 12:10:54 -0400

    add front matter

---
+++ ARGOSTracker.py
@@ -0,0 +1,8 @@
+#-------------------------------------------------------------
+# ARGOSTracker.py
+##
+Description: Parses a line of ARGOS tracking data
+##
+Created by: Jingyi Sun (jingyi.sun@duke.edu)
+# Created on: Fall 2019
+#--------------------------------------------------------------
